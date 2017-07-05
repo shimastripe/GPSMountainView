2d9a0710c0e2ae429b22d26265f0076f9fdc52e9
Go Takagi <takagi@shimastripe.com>
2017-07-06 05:23:27 +0900

    Use google api

--- app/src/main/java/com/shimastripe/gpsmountainview/MainActivity.java
+++ app/src/main/java/com/shimastripe/gpsmountainview/MainActivity.java
@@ -1,7 +1,8 @@
 package com.shimastripe.gpsmountainview;
 
 import android.Manifest;
-import android.content.Intent;
+import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -9,14 +10,14 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.location.LocationProvider;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -26,6 +27,16 @@ import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,7 +47,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity implements LocationListener, SensorEventListener {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener,
+        ResultCallback<LocationSettingsResult>, SensorEventListener {
 
     private final static String TAG = "MainActivity";
 
@@ -49,9 +61,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     private float alturaV;
     private float alturaH;
 
-    private LocationManager locationManager;
-    private double latitude;
-    private double longtitude;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private Location mCurrentLocation;
+    protected LocationSettingsRequest mLocationSettingsRequest;
+    private final static int REQCODE_PERMISSIONS = 0x1;
+
     private TextView textView1, textView2, textView3;
 
     private LineChart lineChart;
@@ -66,11 +81,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         textView2 = (TextView) findViewById(R.id.text_view2);
         textView3 = (TextView) findViewById(R.id.text_view3);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-        } else {
-            locationStart();
-        }
+        buildGoogleApiClient();
+        createLocationRequest();
+        buildLocationSettingsRequest();
 
         sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -88,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             finish();
             return;
         }
-        lineChart = (LineChart)findViewById(R.id.chart1);
+        lineChart = (LineChart) findViewById(R.id.chart1);
     }
 
     @Override
@@ -97,6 +110,25 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         Log.i(TAG, "onResume");
         sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
         sensorMgr.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_FASTEST);
+
+        if (isLocationEnabled(this.getApplicationContext())) {
+            if (mGoogleApiClient.isConnected()) {
+                startLocationUpdates();
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 
     @Override
@@ -106,8 +138,152 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         sensorMgr.unregisterListener(this);
     }
 
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        // If the initial location was never previously requested, we use
+        // FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+        // its value in the Bundle and check for it in onCreate(). We
+        // do not request it again unless the user specifically requests location updates by pressing
+        // the Start Updates button.
+        //
+        // Because we cache the value of the initial location in the Bundle, it means that if the
+        // user launches the activity,
+        // moves to a new location, and then changes the device orientation, the original location
+        // is displayed as the activity is re-created.
+        Log.d(TAG, "onConnected");
+        if (mCurrentLocation == null) {
+            if (ActivityCompat.checkSelfPermission(
+                    this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                updateLocationUI();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspended i" + i);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "onLocationChanged");
+        setLocation();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed " + connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.i(TAG, "All location settings are satisfied.");
+                startLocationUpdates();
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to" +
+                        "upgrade location settings ");
+
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+                    status.startResolutionForResult(this, REQCODE_PERMISSIONS);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.i(TAG, "PendingIntent unable to execute request.");
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog " +
+                        "not created.");
+                break;
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(5000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    public static boolean isLocationEnabled(Context context) {
+        int locationMode = 0;
+        String locationProviders;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        } else {
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+            }
+            return;
+        } else {
+        }
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(Status status) {
+                if (status.isSuccess()) {
+                    Log.d(TAG, "App Indexing API: Recorded recipe view end successfully." + status.toString());
+                } else {
+                    Log.d(TAG, "App Indexing API: There was an error recording the recipe view."
+                            + status.toString());
+                }
+            }
+        });
+
+    }
+
+    private void updateLocationUI() {
+        if (mCurrentLocation != null) {
+            setLocation();
+        } else {
+            startLocationUpdates();
+        }
+    }
+
     public void onClickStartButton(View view) {
-        Log.d(TAG, "onCliclStartButton()");
+        Log.d(TAG, "onClickStartButton()");
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(DocomoAPIInterface.END_POINT)
@@ -115,7 +291,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                 .build();
 
         DocomoAPIInterface service = retrofit.create(DocomoAPIInterface.class);
-        service.getMountainData(latitude, longtitude, azimuth, alturaH, 45, getString(R.string.DOCOMO_API_KEY)).enqueue(new Callback<MountainRepository>() {
+        service.getMountainData(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude(), azimuth, alturaH, 45, getString(R.string.DOCOMO_API_KEY)).enqueue(new Callback<MountainRepository>() {
             @Override
             public void onResponse(Call<MountainRepository> call, Response<MountainRepository> response) {
                 Log.d(TAG, "Succeed to request");
@@ -127,7 +303,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                         entries.add(new Entry(i, list.get(i)));
                     }
                     //データをセット
-                    LineDataSet dataSet = new LineDataSet(entries,"weight");
+                    LineDataSet dataSet = new LineDataSet(entries, "weight");
                     //LineDataインスタンス生成
                     LineData data = new LineData(dataSet);
                     //LineDataをLineChartにセット
@@ -144,92 +320,17 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             @Override
             public void onFailure(Call<MountainRepository> call, Throwable t) {
                 Log.d(TAG, "Failed to request");
-                Log.d(TAG,t.toString());
+                Log.d(TAG, t.toString());
             }
         });
     }
 
-    private void locationStart() {
-        Log.d(TAG, "locationStart()");
-
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-        if (!gpsEnabled) {
-            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(settingsIntent);
-            Log.d(TAG, "not gpsEnable, startActivity");
-        } else {
-            Log.d(TAG, "gpsEnabled");
+    private void setLocation() {
+        Log.d(TAG, "setLocation");
+        if (mCurrentLocation != null) {
+            textView1.setText("Latitude:" + mCurrentLocation.getLatitude());
+            textView2.setText("Longitude:" + mCurrentLocation.getLongitude());
         }
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
-            Log.d(TAG, "checkSelfPermission false");
-            return;
-        }
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 50, this);
-        Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        latitude = loc.getLatitude();
-        longtitude = loc.getLongitude();
-        setLocation(latitude, longtitude);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == 1000) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.d(TAG, "checkSelfPermission true");
-                locationStart();
-                return;
-
-            } else {
-                Toast toast = Toast.makeText(this, "これ以上なにもできません", Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        }
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        switch (status) {
-            case LocationProvider.AVAILABLE:
-                Log.d(TAG, "LocationProvider.AVAILABLE");
-                break;
-            case LocationProvider.OUT_OF_SERVICE:
-                Log.d(TAG, "LocationProvider.OUT_OF_SERVICE");
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                Log.d(TAG, "LocationProvider.TEMPORARILY_UNAVAILABLE");
-                break;
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged");
-        latitude = location.getLatitude();
-        longtitude = location.getLongitude();
-        setLocation(latitude, longtitude);
-    }
-
-    private void setLocation(double latitude, double longtitude) {
-        textView1.setText("Latitude:" + latitude);
-        textView2.setText("Longitude:" + longtitude);
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        // Called when the provider is enabled by the user.
-        Log.d(TAG, "onProviderEnabled()");
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        // Called when the provider is disabled by the user.
-        Log.d(TAG, "onProviderDisabled");
     }
 
     @Override
